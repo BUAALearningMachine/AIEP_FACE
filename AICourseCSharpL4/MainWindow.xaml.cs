3020e87775c2fa722ef7a8a90ffdfaf7ad49fade
samyunwei <259375702@qq.com>
2019-11-19 01:34:38 +0800

    添加了一下初始化函数

--- AICourseCSharpL4/MainWindow.xaml.cs
+++ AICourseCSharpL4/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace AICourseCSharpL4
 {
@@ -20,12 +22,75 @@ namespace AICourseCSharpL4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Baidu.Aip.Face.Face mClient;
+        private IniFile mConfig;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        void init()
+        {
+
+        }
+
+        void initConfig()
+        {
+            mConfig = new IniFile("./cfg.ini");
+        }
+
+        void initClient()
+        { 
+            var APP_ID = mConfig.IniReadValue("Baidu", "APP_ID").ToString(); 
+            var API_KEY = mConfig.IniReadValue("Baidu", "API_KEY").ToString();;
+            var SECRET_KEY = mConfig.IniReadValue("Baidu", "SECRET_KEY").ToString();
+            mClient = new Baidu.Aip.Face.Face(API_KEY, SECRET_KEY);
+            mClient.Timeout = 60000;  // 修改超时时间
+        }
+
+        void initGui()
+        {
+
+        }
+
+        void initDict()
+        {
+
+        }
+
+        private void button_recongnition2_Click(object sender, RoutedEventArgs e)
+        {
+            string pathImg = "";
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "选择识别语音文件";
+            //fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "图片文件(*.jpg,*.jpeg,*.png,*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+
+            fdlg.FilterIndex = 0;
+            fdlg.RestoreDirectory = false;
+            if (fdlg.ShowDialog() == true)
+            {
+                pathImg = fdlg.FileName;
+            }
+        }
+
+        private void button_recongnition1_Click(object sender, RoutedEventArgs e)
+        {
+            string pathImg = "";
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "选择识别语音文件";
+            //fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "图片文件(*.jpg,*.jpeg,*.png,*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+
+            fdlg.FilterIndex = 0;
+            fdlg.RestoreDirectory = false;
+            if (fdlg.ShowDialog() == true)
+            {
+                 pathImg = fdlg.FileName;
+            }
+        }
+
+        private void button_diff_Click(object sender, RoutedEventArgs e)
         {
 
         }
