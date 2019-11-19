f1d8c63309241aeebd89aefdc58344d5e3450556
samyunwei <259375702@qq.com>
2019-11-20 01:25:03 +0800

    添加接口

--- AICourseCSharpL4/MainWindow.xaml.cs
+++ AICourseCSharpL4/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,11 +28,13 @@ namespace AICourseCSharpL4
         public MainWindow()
         {
             InitializeComponent();
+            init();
         }
 
         void init()
         {
-
+            initConfig();
+            initClient();
         }
 
         void initConfig()
@@ -44,6 +47,8 @@ namespace AICourseCSharpL4
             var APP_ID = mConfig.IniReadValue("Baidu", "APP_ID").ToString(); 
             var API_KEY = mConfig.IniReadValue("Baidu", "API_KEY").ToString();;
             var SECRET_KEY = mConfig.IniReadValue("Baidu", "SECRET_KEY").ToString();
+            Console.WriteLine(API_KEY);
+            Console.WriteLine(SECRET_KEY);
             mClient = new Baidu.Aip.Face.Face(API_KEY, SECRET_KEY);
             mClient.Timeout = 60000;  // 修改超时时间
         }
@@ -72,6 +77,27 @@ namespace AICourseCSharpL4
             {
                 pathImg = fdlg.FileName;
             }
+
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(pathImg, UriKind.RelativeOrAbsolute);
+            bi.EndInit();
+            image_recongnition2.Source = bi;
+
+            string image = Convert.ToBase64String(File.ReadAllBytes(pathImg));
+
+
+            // 调用人脸检测，可能会抛出网络等异常，请使用try/catch捕获
+            // 如果有可选参数
+            var options = new Dictionary<string, object>{
+                {"face_field", "beauty,age,expression,face_shape,gender,race"},
+            };
+
+
+            // 带参数调用人脸检测
+            var result = mClient.Detect(image, "BASE64", options);
+            textBox_recongnition2.Text = result.ToString();
+
         }
 
         private void button_recongnition1_Click(object sender, RoutedEventArgs e)
@@ -88,6 +114,24 @@ namespace AICourseCSharpL4
             {
                  pathImg = fdlg.FileName;
             }
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(pathImg, UriKind.RelativeOrAbsolute);
+            bi.EndInit();
+            image_recongnition1.Source = bi;
+            string image = Convert.ToBase64String(File.ReadAllBytes(pathImg));
+
+
+            // 调用人脸检测，可能会抛出网络等异常，请使用try/catch捕获
+            // 如果有可选参数
+            var options = new Dictionary<string, object>{
+                {"face_field", "beauty,age,expression,face_shape,gender,race"},
+            };
+
+
+            // 带参数调用人脸检测
+            var result = mClient.Detect(image,"BASE64",options);
+            textBox_recongnition1.Text = result.ToString();
         }
 
         private void button_diff_Click(object sender, RoutedEventArgs e)
